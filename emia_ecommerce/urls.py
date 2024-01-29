3b75a47b90dcefa6d3b1d885ad01771a74edf1f6
Kwaku Boateng <105262423+kwakuboateng-dev@users.noreply.github.com>
2024-01-29 02:48:20 +0000

    Update urls.py

--- emia_ecommerce/urls.py
+++ emia_ecommerce/urls.py
@@ -14,16 +14,3 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
-
-
-feat(api): configure API routes and documentation using Django Rest Framework and drf-spectacular
-
-This commit sets up API routes for the 'Category' model using Django Rest Framework's DefaultRouter. Additionally, it integrates drf-spectacular to generate API schemas and documentation endpoints.
-
-Summary of Changes:
-- Configured a DefaultRouter for 'Category' views in the 'product' app.
-- Defined API routes under '/api/' to include CRUD operations for 'Category' instances.
-- Added 'SpectacularAPIView' and 'SpectacularSwaggerView' for API schema generation and interactive documentation.
-- Enhanced the project's API capabilities and documentation features.
-
-These changes improve the organization and accessibility of API endpoints while providing comprehensive documentation through drf-spectacular.
